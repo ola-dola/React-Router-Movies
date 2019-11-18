16199e2fe62c730a93f6eb103be432d30089e530
oredola <oredolaolamide@gmail.com>
2019-11-19 00:56:15 +0100

    Added save movie functionality

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ const App = () => {
       <SavedList list={savedList} />
       
       <Route exact path="/" component={MovieList} />
-      <Route  path="/movies/:id" component={Movie} />
+      <Route  path="/movies/:id" render={props => <Movie {...props} addToSavedList={addToSavedList} />} />
     </div>
   );
 };
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MovieCard = props => {
 
   const { title, director, metascore, stars } = props.movie;
-  
+
   return (
     <div className="save-wrapper">
       <div className="movie-card">
@@ -22,7 +22,7 @@ const MovieCard = props => {
           </div>
         ))}
       </div>
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={props.saveMovie}>Save</div>
     </div>
   );
 };
